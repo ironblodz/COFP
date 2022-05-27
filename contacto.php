6864443be15238878a5c91e37464ca8426e20afc
ironblodz <naylestorm12@gmail.com>
2022-05-27 09:50:56 +0100

    update

--- contacto.php
+++ contacto.php
@@ -29,7 +29,9 @@ if($result-> num_rows >0) {
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
+        rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -48,7 +50,8 @@ if($result-> num_rows >0) {
 
 <body>
     <!-- Spinner Start -->
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
             <span class="sr-only">Loading...</span>
         </div>
@@ -58,26 +61,30 @@ if($result-> num_rows >0) {
 
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="login.php">
-            <img  src="img/logo.png" alt="" width="35%" style="padding-left:10px"/>
-        </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <div class="navbar-nav ms-auto p-4 p-lg-0">
-          
-            <a href="formacoes.html" class="nav-item nav-link">Formações</a>
-            <a href="#" class="nav-item nav-link">Workshops</a>
-            <a href="#" class="nav-item nav-link">Candidaturas</a>
-            <a href="contacto.html" class="nav-item nav-link">Contacto</a>
-            <a href="#" class="nav-item nav-link">FAQ</a>
-          </div>
-            <a href="login.html" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block"
-              >Login<i class="fa fa-arrow-right ms-3"></i
-            ></a>
-          </div>
-    </nav>
+      <a href="index.html">
+          <img  src="img/logo.png" alt="" width="35%" style="padding-left:10px"/>
+      </a>
+    <button
+      type="button"
+      class="navbar-toggler me-4"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarCollapse"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div class="navbar-nav ms-auto p-4 p-lg-0">
+        <a href="formacoes.php" class="nav-item nav-link">Formações</a>
+        <a href="workshops.php" class="nav-item nav-link">Workshops online</a>
+        <a href="formulario.php" class="nav-item nav-link">Candidaturas</a>
+        <a href="contacto.php" class="nav-item nav-link">Contactos</a>
+        <a href="faq.php" class="nav-item nav-link">FAQ</a>
+      </div>
+      <a href="login.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block"
+        >Login<i class="fa fa-arrow-right ms-3"></i
+      ></a>
+    </div>
+  </nav>
     <!-- Navbar End -->
 
 
@@ -87,7 +94,7 @@ if($result-> num_rows >0) {
             <div class="row justify-content-center">
                 <div class="col-lg-10 text-center">
                     <h1 class="display-3 text-white animated slideInDown">Contacto</h1>
-                  
+
                 </div>
             </div>
         </div>
@@ -107,7 +114,8 @@ if($result-> num_rows >0) {
                     <h5>Entrar em contato</h5>
                     <p class="mb-4">Caso tenha alguma dúvida, não hesite em contactar</a>.</p>
                     <div class="d-flex align-items-center mb-3">
-                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary"
+                            style="width: 50px; height: 50px;">
                             <i class="fa fa-map-marker-alt text-white"></i>
                         </div>
                         <div class="ms-3">
@@ -116,7 +124,8 @@ if($result-> num_rows >0) {
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-3">
-                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary"
+                            style="width: 50px; height: 50px;">
                             <i class="fa fa-phone-alt text-white"></i>
                         </div>
                         <div class="ms-3">
@@ -125,7 +134,8 @@ if($result-> num_rows >0) {
                         </div>
                     </div>
                     <div class="d-flex align-items-center">
-                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary"
+                            style="width: 50px; height: 50px;">
                             <i class="fa fa-envelope-open text-white"></i>
                         </div>
                         <div class="ms-3">
@@ -163,7 +173,8 @@ if($result-> num_rows >0) {
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Escreva a tua mensagem aqui" id="message" style="height: 150px"></textarea>
+                                    <textarea class="form-control" placeholder="Escreva a tua mensagem aqui"
+                                        id="message" style="height: 150px"></textarea>
                                     <label for="message">Messagem</label>
                                 </div>
                             </div>
@@ -181,40 +192,41 @@ if($result-> num_rows >0) {
 
     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-      <div class="container py-5">
-          <div class="row g-5">
-              <div class="col-lg-3 col-md-6">
-                  <h4 class="text-white mb-3">Formações</h4>
-                  <a class="btn btn-link" href="">Workshops Online</a>
-                  <a class="btn btn-link" href="">Candidaturas</a>
-                  <a class="btn btn-link" href="">Contactos</a>
-                  <a class="btn btn-link" href="">FAQ</a>
-                  <a class="btn btn-link" href="">Login</a>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <h4 class="text-white mb-3">Contacto</h4>
-                  <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Rua Miguel Torga, Leiria</p>
-                  <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+351 923 021 021</p>
-                  <p class="mb-2"><i class="fa fa-envelope me-3"></i>INFOphelp@gmail.com</p>
-                  <div class="d-flex pt-2">
-                      <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                      <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                      <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                      <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <h4 class="text-white mb-3">Newsletter</h4>
-                  <p>Se quiser receber novidades sobre as nossas formações e workshops, subscrevam </p>
-                  <div class="position-relative mx-auto" style="max-width: 400px;">
-                      <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                      <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Subscrever</button>
-                  </div>
-              </div>
-          </div>
-      </div>
-      
-  </div>
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Formações</h4>
+                    <a class="btn btn-link" href="">Workshops Online</a>
+                    <a class="btn btn-link" href="">Candidaturas</a>
+                    <a class="btn btn-link" href="">Contactos</a>
+                    <a class="btn btn-link" href="">FAQ</a>
+                    <a class="btn btn-link" href="">Login</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Contacto</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Rua Miguel Torga, Leiria</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+351 923 021 021</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>INFOphelp@gmail.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Newsletter</h4>
+                    <p>Se quiser receber novidades sobre as nossas formações e workshops, subscrevam </p>
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                        <button type="button"
+                            class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Subscrever</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
     <!-- Footer End -->
 
 
