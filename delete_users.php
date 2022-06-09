2ec198e9afbbfeea8d92ea521d81a91fcc4b9223
ironblodz <naylestorm12@gmail.com>
2022-06-09 14:27:37 +0100

    update

--- delete_users.php
+++ delete_users.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
 include("connection.php");
+$id_utilizador=htmlspecialchars(  $_POST['id_utilizador']);
 
-$sql = "DELETE FROM utilizador WHERE id_utilizador= ?";
-
-if ($conn->query($sql) === TRUE) {
+$sql = "DELETE FROM utilizador WHERE id_utilizador= $id_utilizador";
+$resulte=$conn->query($sql);
+if ($resulte && $conn->affected_rows) {
   echo "Foi eliminado com sucesso";
 } else {
   echo "Erro ao excluir registro: " . $conn->error;
 }
 
 $conn->close();
+
