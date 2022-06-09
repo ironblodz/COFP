997b1383bcec21aff2e450831fb9bbe8b8e5ddc9
rodrig02 <rod.rodrigues2002dez@gmail.com>
2022-06-09 16:22:01 +0100

    update

--- delete_formacoes.php
+++ delete_formacoes.php
@@ -1,13 +1,20 @@
 <?php
 session_start();
 include("connection.php");
+if(isset($_POST['id_formacao'])){
+$id_formacao=htmlspecialchars($_POST['id_formacao']);
 
-$sql = "DELETE FROM formacao WHERE id_formacao= ?";
+$sql = "DELETE FROM formacao WHERE id_formacao=$id_formacao";
 
-if ($conn->query($sql) === TRUE) {
+$resulte=$conn->query($sql);
+if ($resulte && $conn->affected_rows) {
   echo "Foi eliminado com sucesso";
 } else {
   echo "Erro ao excluir registro: " . $conn->error;
 }
 
+header('location:admin/listaformacoes.php');
+
 $conn->close();
+
+}
