521223d8d1d9830da30e6efbfb9942108e7c365b
AlexandreOSilva <alexandreosilva@outlook.pt>
2022-06-09 15:13:51 +0100

    up

--- admin/users.php
+++ admin/users.php
@@ -321,7 +321,7 @@ $result = $conn->query($query);
         <!-- Footer Start -->
         <div class="container-fluid pt-4 px-4">
           <div class="bg-light rounded-top p-4">
-            <div class="row">
+            <div class="row">gdr
               <div class="col-12 col-sm-6 text-center text-sm-start">
                 &copy; <a href="#">INFO</a>, Todos os direitos Reservados.
               </div>
